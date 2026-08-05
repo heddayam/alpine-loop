6228025ba4e471a5b3cb4142ff6536858ccae99a
Mourad Heddaya <mourad@privacylabs.io>
2026-08-04 22:22:40 -0700

    feat: add target-directed closed tour search

--- lib/solver/index.ts
+++ lib/solver/index.ts
@@ -1,6 +1,7 @@
 export * from "./budget";
 export * from "./candidate";
 export * from "./canonical";
+export * from "./closed-tours";
 export * from "./control";
 export * from "./compressed-graph";
 export * from "./diversity";
