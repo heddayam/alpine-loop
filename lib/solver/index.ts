cad1e8fe7fb7e38de964178a7cc9cc40e3dcd5c6
Mourad Heddaya <mourad@privacylabs.io>
2026-08-04 22:21:10 -0700

    feat: add metric-preserving graph compression

--- lib/solver/index.ts
+++ lib/solver/index.ts
@@ -2,6 +2,7 @@ export * from "./budget";
 export * from "./candidate";
 export * from "./canonical";
 export * from "./control";
+export * from "./compressed-graph";
 export * from "./diversity";
 export * from "./generate";
 export * from "./multi-start-solver";
