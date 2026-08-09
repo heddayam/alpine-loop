64cd29dc697e5d20a1ec19602128f48d0445c301
heddayam <27699190+heddayam@users.noreply.github.com>
2026-08-08 17:39:15 -0700

    Add Henry Coe regional pack pipeline

--- lib/data/regional-pack.test.ts
+++ lib/data/regional-pack.test.ts
@@ -4,17 +4,19 @@ import { listRegionalPackBuilderIds, requireRegionalPackBuilder } from "./region
 describe("regional pack builder registry", () => {
   it("registers active builders in stable pack-ID order", () => {
     expect(listRegionalPackBuilderIds()).toEqual([
+      "henry-coe",
       "monterey-carmel",
       "santa-cruz-mountains",
       "southern-east-bay",
     ]);
+    expect(requireRegionalPackBuilder("henry-coe")).toEqual(expect.any(Function));
     expect(requireRegionalPackBuilder("monterey-carmel")).toEqual(expect.any(Function));
     expect(requireRegionalPackBuilder("santa-cruz-mountains")).toEqual(expect.any(Function));
     expect(requireRegionalPackBuilder("southern-east-bay")).toEqual(expect.any(Function));
   });
 
   it("distinguishes planned regions from unknown pack ids", () => {
-    expect(() => requireRegionalPackBuilder("henry-coe")).toThrow(/planned.*does not have a pack builder/i);
+    expect(() => requireRegionalPackBuilder("marin-mount-tam")).toThrow(/planned.*does not have a pack builder/i);
     expect(() => requireRegionalPackBuilder("not-a-region")).toThrow(/unknown regional pack/i);
   });
 });
